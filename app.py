b57c4760d66d0f810c425674715ad3666fa1290a
Horacjusz <pawel.prus05@gmail.com>
2024-11-14 06:07:15 +0100

    for render3

--- app.py
+++ app.py
@@ -25,5 +25,6 @@ app.register_blueprint(debug_blueprint)
 
 if __name__ == "__main__":
     
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
     # app.run(debug=True)
